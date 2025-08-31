c921e90c3a8d01dcd368e69d7acafbc9b6148b1e
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-08-30 19:29:29 -0500

    Add dinamic pages and reservation confirmation

---
+++ frontend/app/src/services/products.ts
@@ -0,0 +1,15 @@
+//Conect to Firestore
+import { db } from "./firebase";
+import { getDocs, collection, getDoc, doc } from "firebase/firestore";
+
+//Get all products
+export const getAllProducts = async () => {
+  const products = await getDocs(collection(db, "products"));
+  return products.docs.map((doc) => doc.data());
+};
+
+//Get product by id
+export const getProductById = async (id: string) => {
+  const product = await getDoc(doc(db, "products", id));
+  return product.data();
+};
