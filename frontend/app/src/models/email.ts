252f61eb8a5fc83d83fc9c9ed0e0a9cd098cdbf5
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-09-05 18:02:09 -0600

    Add email notification functionality

---
+++ frontend/app/src/models/email.ts
@@ -0,0 +1,20 @@
+export interface EmailData {
+  logo?: string;
+  email: string;
+  order_id: string;
+  orders: Order[];
+  cost: {
+    total: string;
+  };
+}
+
+interface Order {
+  image_url: string;
+  capacity: string;
+  reserved_by: string;
+  reservation_date: string;
+  reservation_time: string;
+  price_per_hour: string;
+  details: string[];
+  total_price: string;
+}
